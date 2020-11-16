4c53a27ff1a1df95775deb524d30d7719c534774
Jacob Benedetti <jacob.benedetti@uconn.edu>
2020-11-16 17:08:24 -0500

    First draft of classes.

---
+++ ITS.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class ITS {
+    private JLabel integratedTravelSystemLabel;
+    private JRadioButton createProfileRadioButton;
+    private JRadioButton deleteProfileRadioButton;
+    private JButton selectButton;
+    private JRadioButton updateProfileRadioButton;
+    private JRadioButton findDisplayProfileRadioButton;
+    private JRadioButton displayAllProfilesRadioButton;
+}
